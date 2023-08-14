d76ed1652f54667722e35f14de03d5cbbbafcdb7
Joana <asiakom24@vp.pl>
2023-08-14 09:10:40 +0200

    plots e3_1d

--- E3_1d_plot.py
+++ E3_1d_plot.py
@@ -12,9 +12,9 @@ for n_c in n_centroids:
         dataset_names.append('snake_c%i_f%i' % (n_c, f))
 
 print(dataset_names, len(dataset_names))
+
 ###
 
-res = np.load('results/E3_1d.npy')
 res_pred = np.load('results/E3_1d_v.npy')
 
 res_pred = np.mean(res_pred, axis=0)
@@ -30,9 +30,9 @@ cols=['black', 'cornflowerblue','cornflowerblue','cornflowerblue','tomato','toma
 
 for d_id, d in enumerate(dataset_names):
     for est_id, est in enumerate(labels):
-        if est_id in [2,3,4,6,7]:
+        if est_id in [2,1,4,6,7]:
             continue
-        ax[d_id].plot(res_pred[d_id, est_id], label=est, ls=ls[est_id], c=cols[est_id])
+        ax[d_id].plot(res_pred[d_id, est_id], label=est, ls=ls[est_id], c=cols[est_id], alpha=0.7)
     
     ax[d_id].set_title(d)
     ax[d_id].grid(ls=':')
@@ -42,3 +42,25 @@ ax[0].legend(frameon=False)
 plt.tight_layout()
 plt.savefig('figures/E3_1d.png')
 
+
+###
+
+res = np.load('results/E3_1d.npy')
+res = np.mean(res, axis=0)[:,:,-1,] # last iteration
+
+
+# Plot imgs
+labels = ['KDE-g', 'KDE-t', 'KDE-e', 'DPL-none', 'DPL-sqrt', 'DPL-log', 'DPL-std_norm']
+
+fig, ax = plt.subplots(1,3,figsize=(10,7), sharex=True, sharey=True)
+
+for m_id, m in enumerate(['Statistic', 'MSE', 'MAE']):
+    ax[m_id].imshow(res[:,:,m_id], cmap='coolwarm')
+    
+    ax[m_id].set_title(m)
+    ax[m_id].grid(ls=':')
+    ax[m_id].set_xticks(np.arange(len(labels)), labels, rotation=90)
+    ax[m_id].set_yticks(np.arange(len(dataset_names)), dataset_names)
+            
+plt.tight_layout()
+plt.savefig('figures/E3_1d_err.png')
