203eb2d52c1a85ed10ba6fe0be0e7e7ea2c7c06a
Joana <asiakom24@vp.pl>
2023-08-28 19:51:39 +0200

    plts

--- E1_est_plot.py
+++ E1_est_plot.py
@@ -26,34 +26,34 @@ print(res.shape) # factors x centroids x cq  x reg x transforms x iters
 res = res[..., -1] # last iteration
 print(res.shape) # cq x integrators x reg x transforms
 
-# for factor_id, factor in enumerate(factors):
+for factor_id, factor in enumerate(factors):
 
-#     fig, ax = plt.subplots(5,6,figsize=(12,12), sharex=True, sharey=True)
-#     plt.suptitle('factor: %i' % factor)
+    fig, ax = plt.subplots(5,6,figsize=(12,12), sharex=True, sharey=True)
+    plt.suptitle('factor: %i' % factor)
     
-#     for cent_id, cent in enumerate(n_centroids):
-#         for r_id, r in enumerate(base_regressors):
+    for cent_id, cent in enumerate(n_centroids):
+        for r_id, r in enumerate(base_regressors):
             
-#             if cent_id==0:
-#                 ax[cent_id, r_id].set_title('%s' % (r))
-#                 ax[-1, r_id].set_xlabel('curve quants')
+            if cent_id==0:
+                ax[cent_id, r_id].set_title('%s' % (r))
+                ax[-1, r_id].set_xlabel('curve quants')
 
-#             if r_id==0:
-#                 ax[cent_id, r_id].set_ylabel('%i centroids \n transform' % cent)
+            if r_id==0:
+                ax[cent_id, r_id].set_ylabel('%i centroids \n transform' % cent)
                 
-#             ax[cent_id, r_id].imshow(res[factor_id, cent_id, :, r_id, :],cmap='coolwarm')
+            ax[cent_id, r_id].imshow(res[factor_id, cent_id, :, r_id, :],cmap='coolwarm', vmin=0, vmax=0.5)#np.max(res[factor_id]))
             
-#             ax[cent_id, r_id].set_xticks(np.arange(4), curve_quants, rotation=90)
-#             ax[cent_id, r_id].set_yticks(np.arange(4), transforms)
+            ax[cent_id, r_id].set_xticks(np.arange(4), curve_quants, rotation=90)
+            ax[cent_id, r_id].set_yticks(np.arange(4), transforms)
             
-#             for _a, __a in enumerate(curve_quants):
-#                 for _b, __b in enumerate(transforms):
-#                     ax[cent_id, r_id].text(_b, _a, "%.3f" % (
-#                         res[factor_id, cent_id, _a, r_id, _b]
-#                         ) , va='center', ha='center', c='white', fontsize=8)
+            for _a, __a in enumerate(curve_quants):
+                for _b, __b in enumerate(transforms):
+                    ax[cent_id, r_id].text(_b, _a, "%.3f" % (
+                        res[factor_id, cent_id, _a, r_id, _b]
+                        ) , va='center', ha='center', c='white', fontsize=8)
             
-#         plt.tight_layout()
-#         plt.savefig('figures/E1_est_mse_f%i.png' % factor)
+        plt.tight_layout()
+        plt.savefig('figures/E1_est_mse_f%i.png' % factor)
         
 # preds
 res_pred = np.load('results/E1_est_v.npy')
@@ -65,8 +65,9 @@ res_pred = np.mean(res_pred, axis=0)
 
 print(res_pred.shape) # factors x centroids x cq x reg x transforms
 
-cq_id=2
-cols=plt.cm.Reds(np.linspace(0.2,1,len(base_regressors)))
+cq_id=1
+# cols=plt.cm.magma(np.linspace(0,1,len(base_regressors)))
+cols=['blue', 'pink', 'forestgreen', 'orange', 'cyan', 'red']
 
 fig, ax = plt.subplots(5, 4, figsize=(13,13), sharex=True, sharey=True)
 
@@ -74,7 +75,7 @@ for cent_id, cent in enumerate(n_centroids):
     for tr_id, tr in enumerate(transforms):
 
         if tr_id==0:
-            ax[cent_id, tr_id].set_ylabel('%i centtroids' % cent)
+            ax[cent_id, tr_id].set_ylabel('%i centroids' % cent)
         if cent_id==0:
             ax[cent_id, tr_id].set_title('%s' % (tr))
         
@@ -83,7 +84,7 @@ for cent_id, cent in enumerate(n_centroids):
             ax[cent_id, tr_id].plot(test_space, res_pred[cent_id, cq_id, r_id, tr_id], label='%s' % (r), alpha=0.5, c=cols[r_id])
         
         ax[cent_id, tr_id].grid(ls=':')
-        ax[cent_id, tr_id].plot(test_space, np.mean(originals[:, cent_id], axis=0), color='blue', ls=':')
+        ax[cent_id, tr_id].plot(test_space, np.mean(originals[:, cent_id], axis=0), color='black', ls=':')
 
 
 plt.legend(loc='lower center', bbox_to_anchor=(-1.4, -0.6), ncol=3, frameon=False)
