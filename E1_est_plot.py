7a6761081b7605c80f533538c99a3e51839d68fe
Joana <asiakom24@vp.pl>
2023-08-28 18:28:53 +0200

    e1 est plots v3

--- E1_est_plot.py
+++ E1_est_plot.py
@@ -68,28 +68,29 @@ print(res_pred.shape) # factors x centroids x cq x reg x transforms
 cq_id=2
 cols=plt.cm.Reds(np.linspace(0.2,1,len(base_regressors)))
 
+fig, ax = plt.subplots(5, 4, figsize=(12,12), sharex=True, sharey=True)
+
 for cent_id, cent in enumerate(n_centroids):
-        
-    fig, ax = plt.subplots(2, 2, figsize=(12,12), sharex=True, sharey=True)
-    ax = ax.ravel()
-    
     for tr_id, tr in enumerate(transforms):
 
-        ax[tr_id].set_title(tr)
+        if tr_id==0:
+            ax[cent_id, tr_id].set_ylabel('%i centtroids' % cent)
+        if cent_id==0:
+            ax[cent_id, tr_id].set_title('%s' % (tr))
         
         # for cq_id, cq in enumerate(curve_quants):
         for r_id, r in enumerate(base_regressors):
-            ax[tr_id].plot(res_pred[cent_id, cq_id, r_id, tr_id], label='%s' % (r), alpha=0.5, c=cols[r_id])
+            ax[cent_id, tr_id].plot(res_pred[cent_id, cq_id, r_id, tr_id], label='%s' % (r), alpha=0.5, c=cols[r_id])
         
-        ax[tr_id].grid(ls=':')
-        ax[tr_id].plot(originals[0, cent_id], color='blue', ls='--')
+        ax[cent_id, tr_id].grid(ls=':')
+        ax[cent_id, tr_id].plot(np.mean(originals[:, cent_id], axis=0), color='blue', ls=':')
 
 
-    ax[0].legend()
-    plt.tight_layout()
-    plt.savefig('figures/E1_est_c%i.png' % (cent))
-                    
-            
+plt.legend(ncol=1, frameon=False)
+plt.tight_layout()
+plt.savefig('figures/E1_est.png')
+                
+        
         
         
         
